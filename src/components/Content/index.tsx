48524c01e9dfa9e93b3f3f4b9727cda05900ef00
Andrelino Silva <andrelinocom@gmail.com>
2021-08-06 08:00:50 -0300

    Refatorando o código

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,67 @@
+import { useEffect, useState } from 'react';
+import { MovieCard } from '../MovieCard';
+
+import { api } from '../../services/api';
+
+import './styles.scss';
+
+type ContentProps = {
+    selectedGenreId: number;
+};
+
+interface GenreContentProps {
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+}
+
+interface MovieProps {
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+        Source: string;
+        Value: string;
+    }>;
+    Runtime: string;
+}
+
+export function Content({ selectedGenreId }: ContentProps) {
+    const [movies, setMovies] = useState<MovieProps[]>([]);
+    const [selectedGenre, setSelectedGenre] = useState<GenreContentProps>(
+        {} as GenreContentProps
+    );
+
+    useEffect(() => {
+        api.get(`movies/?Genre_id=${selectedGenreId}`).then((response) => {
+            setMovies(response.data);
+        });
+
+        api.get(`genres/${selectedGenreId}`).then((response) => {
+            setSelectedGenre(response.data);
+        });
+    }, [selectedGenreId]);
+
+    return (
+        <div className="container">
+            <header>
+                <span className="category">
+                    Categoria:<span> {selectedGenre.title}</span>
+                </span>
+            </header>
+
+            <main>
+                <div className="movies-list">
+                    {movies.map((movie) => (
+                        <MovieCard
+                            key={movie.imdbID}
+                            title={movie.Title}
+                            poster={movie.Poster}
+                            runtime={movie.Runtime}
+                            rating={movie.Ratings[0].Value}
+                        />
+                    ))}
+                </div>
+            </main>
+        </div>
+    );
+}
