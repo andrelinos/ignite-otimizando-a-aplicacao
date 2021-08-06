07e718f0155e074514a6df74bf7c319680895068
Andrelino Silva <andrelinocom@gmail.com>
2021-08-06 08:17:10 -0300

    Instalando ESLint, Prettier

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -26,42 +26,42 @@ interface MovieProps {
 }
 
 export function Content({ selectedGenreId }: ContentProps) {
-    const [movies, setMovies] = useState<MovieProps[]>([]);
-    const [selectedGenre, setSelectedGenre] = useState<GenreContentProps>(
-        {} as GenreContentProps
-    );
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+  const [selectedGenre, setSelectedGenre] = useState<GenreContentProps>(
+        {} as GenreContentProps,
+  );
 
-    useEffect(() => {
-        api.get(`movies/?Genre_id=${selectedGenreId}`).then((response) => {
-            setMovies(response.data);
-        });
+  useEffect(() => {
+    api.get(`movies/?Genre_id=${selectedGenreId}`).then((response) => {
+      setMovies(response.data);
+    });
 
-        api.get(`genres/${selectedGenreId}`).then((response) => {
-            setSelectedGenre(response.data);
-        });
-    }, [selectedGenreId]);
+    api.get(`genres/${selectedGenreId}`).then((response) => {
+      setSelectedGenre(response.data);
+    });
+  }, [selectedGenreId]);
 
-    return (
-        <div className="container">
-            <header>
-                <span className="category">
-                    Categoria:<span> {selectedGenre.title}</span>
-                </span>
-            </header>
+  return (
+    <div className="container">
+      <header>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span>
+        </span>
+      </header>
 
-            <main>
-                <div className="movies-list">
-                    {movies.map((movie) => (
-                        <MovieCard
-                            key={movie.imdbID}
-                            title={movie.Title}
-                            poster={movie.Poster}
-                            runtime={movie.Runtime}
-                            rating={movie.Ratings[0].Value}
-                        />
-                    ))}
-                </div>
-            </main>
+      <main>
+        <div className="movies-list">
+          {movies.map((movie) => (
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
+          ))}
         </div>
-    );
+      </main>
+    </div>
+  );
 }
--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -4,53 +4,48 @@ import { Button } from '../Button';
 
 import './styles.scss';
 
-interface GenreResponseProps {
-    id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-    title: string;
-}
-
-interface GenreResponseProps {
+interface GenreContentProps {
     id: number;
     name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
     title: string;
 }
 
 interface SideBarProps {
+    // eslint-disable-next-line no-undef
     setSelectedGenreId: React.Dispatch<React.SetStateAction<number>>;
     selectedGenreId: number;
 }
 
 export function SideBar({ selectedGenreId, setSelectedGenreId }: SideBarProps) {
-    const [genres, setGenres] = useState<GenreResponseProps[]>([]);
+  const [genres, setGenres] = useState<GenreContentProps[]>([]);
 
-    useEffect(() => {
-        api.get<GenreResponseProps[]>('genres').then((response) => {
-            setGenres(response.data);
-        });
-    }, []);
+  useEffect(() => {
+    api.get<GenreContentProps[]>('genres').then((Content) => {
+      setGenres(Content.data);
+    });
+  }, []);
 
-    function handleClickButton(id: number) {
-        setSelectedGenreId(id);
-    }
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
 
-    return (
-        <nav className="sidebar">
-            <span>
-                Watch<p>Me</p>
-            </span>
+  return (
+    <nav className="sidebar">
+      <span>
+        Watch<p>Me</p>
+      </span>
 
-            <div className="buttons-container">
-                {genres.map((genre) => (
-                    <Button
-                        key={String(genre.id)}
-                        title={genre.title}
-                        iconName={genre.name}
-                        onClick={() => handleClickButton(genre.id)}
-                        selected={selectedGenreId === genre.id}
-                    />
-                ))}
-            </div>
-        </nav>
-    );
+      <div className="buttons-container">
+        {genres.map((genre) => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  );
 }
